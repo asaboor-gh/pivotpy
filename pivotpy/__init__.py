0553c9a4e559b06218f546dd71624512c243c7d0
massgh <massgh@users.noreply.github.com>
2020-11-16 17:56:04 -0500

    +++

--- pivotpy/__init__.py
+++ pivotpy/__init__.py
@@ -28,25 +28,3 @@ from matplotlib.pyplot import show,savefig
 
 mpl_imported=['show','savefig']
 __all__.extend(mpl_imported)
-
-
-# Config to Run powershell commands in IPython, You can write this in a file under startup folder too. 
-
-from IPython.core.magic import register_line_cell_magic
-from IPython import get_ipython
-@register_line_cell_magic
-def ps(line, cell=None):
-    if cell:
-        return get_ipython().run_cell_magic('powershell',line,cell)
-    else:
-        get_ipython().run_cell_magic('powershell','--out posh_output',line)
-        return posh_output.splitlines()
-    
-# changing config. You can write it in ipython_config.py under profile.
-
-from traitlets.config.application import get_config
-c = get_config()
-c.ScriptMagics.script_magics.append('powershell')
-c.ScriptMagics.script_paths.update({
-    'powershell' : 'powershell.exe -noprofile -command -',
-    'pwsh': 'pwsh.exe -noprofile -command -'})
