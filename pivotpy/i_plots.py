5b34aa8b0637b6df4ab76df29899c87afe0381b2
massgh <massgh@users.noreply.github.com>
2020-11-23 15:33:07 -0500

    Clean_nbs

--- pivotpy/i_plots.py
+++ pivotpy/i_plots.py
@@ -168,7 +168,8 @@ def plotly_to_html(fig,filename=None,out_string=False):
         - filename : Name of file to save fig. Defualt is None and show plot in Colab/Online or return hrml string.
         - out_string: If True, returns HTML string, if False displays graph if possible.
     """
-    fig_json = fig.to_json()
+    _figure_ = fig # To avoid issue of same namespace
+    fig_json = _figure_.to_json()
     # a simple HTML template
     if filename:
         template = """<html>
